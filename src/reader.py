d432e428399e5b67dcf56a1a4a56c7a595f9fc35
zorro <zqqf16@gmail.com>
2013-08-28 09:37:53 -0700

    read from fil

--- src/reader.py
+++ src/reader.py
@@ -5,11 +5,14 @@ import markdown
 from datetime import datetime
 
 def get_date(string):
+    '''Convert string to datetime.
+       Need to support more formats...
+       ToDo
+    '''
     try:
         date = datetime.strptime(string, '%Y-%m-%d')
     except:
         date = datetime.now()
-
     return date
 
 # Metadata handlers
@@ -22,14 +25,13 @@ META_HANDLERS = {
 }
 
 class Reader(object):
+    '''Read Markdown, return HTML'''
 
     def __init__(self):
         self._md = markdown.Markdown(extensions=['fenced_code', 'codehilite', 'meta'], 
-                                    extension_configs={
-                                        'codehilite': [
-                                            ('guess_lang', False)
-                                        ]
-                                    })
+                                     extension_configs={
+                                         'codehilite': [ ('guess_lang', False) ]
+                                     })
  
     def _parse_metadata(self, meta):
         res = {}
@@ -39,16 +41,18 @@ class Reader(object):
 
         return res
 
-    def read(self, path): 
-        with open(path, 'r') as f: 
-            document = f.read().decode('utf-8') 
-            content = self._md.reset().convert(document.strip(' \n'))
-            meta = self._parse_metadata(self._md.Meta)
-
+    def read(self, string):
+        content = self._md.reset().convert(string.strip(' \n'))
+        meta = self._parse_metadata(self._md.Meta)
         return content, meta
+
+    def read_from_file(self, path):
+        with open(path, 'r') as f:
+            document = f.read().decode('utf-8') 
+        return self.read(document)
 
 if __name__ == '__main__':
     r = Reader()
-    content, meta = r.read('../example.md')
+    content, meta = r.read_from_file('../example.md')
     print(content)
     print(meta)
