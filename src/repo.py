50d610df58f24b972eca6b9ea82f159442c8de6e
zorro <zqqf16@gmail.com>
2013-08-29 03:01:51 -0700

    add repo

---
+++ src/repo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import git
+
+'''
+blob = repo.head.commit.tree['index.txt'] # or use a relative path, like 
+path/to/file
+data = blob.data_stream.read()
+update_data(data)
+'''
+
+
