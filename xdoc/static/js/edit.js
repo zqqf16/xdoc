8a70674537356a946cf0534b7d0161251312015a
zorro <zqqf16@gmail.com>
2013-09-09 08:31:36 -0700

    editor

--- xdoc/static/js/edit.js
+++ xdoc/static/js/edit.js
@@ -1,8 +1,24 @@
-angular.module('doc.ui-ace', ['ui.ace'])
-  .controller('AceCtrl', ['$scope', '$http', function ($scope, $http) {
-	  $http.get('/raw/example.md').success(function(data) {
-		  $scope.aceModel = data.content;
-		  $scope.title = data.title;
-	  });
-  }])
-;
+var editor = angular.module('xdoc.editor', ['ui.ace']);
+
+editor.controller('AceCtrler', ['$scope', '$http', function($scope, $http) {
+	$scope.aceStyle = {'height': '1000px','weight':'800px'};
+	$scope.aceHeight = '1000px';
+	$http.get('/raw/example.md').success(function(data){
+		$scope.aceModel = data.content;
+		$scope.title = data.title;
+	});
+
+}]);
+
+editor.directive('markdown', function () {
+	var converter = new Showdown.converter();
+	return {
+		restrict: 'AE',
+		link: function (scope, element, attrs) {
+			scope.$watch(attrs.ngModel, function (newVal) {
+				var html = converter.makeHtml(newVal);
+				element.html(html);
+			});
+		}
+	};
+});
