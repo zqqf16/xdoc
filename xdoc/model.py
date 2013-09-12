41a17af7db08268a71749898bdf7ffd405cf7e53
zorro <zqqf16@gmail.com>
2013-09-12 09:10:44 -0700

    use mongoenige

--- xdoc/model.py
+++ xdoc/model.py
@@ -2,32 +2,67 @@
 # -*- coding: utf-8 -*-
 
 '''
-    Draft = {
-        "title"
-        "content"
+    Content = {
+        "title": "Content Title",
+        "body": "Main body",
     }
 
     Author = {
-        "name"
-        "password"
-        "salt"
-        "drafts" [{"title" "draft" "date"}]
+        "name": "Author name",
+        "password": "Author password",
+        "salt": "Salt",
+        "drafts": [
+            {
+                "title": "title",
+                "content_id": "content id",
+                "date": "created date"
+            },
+        ],
     }
 
-    Document = {
-        "title"
-        "category"
-        "view_count"
-        "histories": [ {"author" "draft" "date"}, {"author" "draft" "date"}],
+    Doc = {
+        "title": "title",
+        "tags": ["tag1", "tag2"],
+        "category": "category",
+        "view_count": "view count",
+        "histories": [
+            {
+                "author_id": "author id",
+                "content_id": "content id",
+                "date": "date",
+            }, 
+        ],
     }
 '''
 
-import pymongo
+from mongoengine import *
 
-def get_db(name='xdoc', host='localhost', port=27017):
-    client = pymongo.MongoClient(host, port)
-    return client[name]
+def init(name='xdoc', host='127.0.0.1', port=27017):
+    connect(name, host=host, port=port)
 
+class Content(Document):
+    title = StringField()
+    body = StringField()
 
+class Draft(EmbeddedDocument):
+    title = StringField()
+    content_id = ObjectIdField()
+    date = DateTimeField()
 
+class Author(Document):
+    name = StringField(primary_key=True)
+    password = StringField()
+    salt = StringField()
+    drafts = ListField(EmbeddedDocumentField(Draft))
 
+class Version(EmbeddedDocument):
+    author_id = ObjectIdField()
+    content_id = ObjectIdField()
+    date = DateTimeField()
+
+class Doc(Document):
+    title = StringField()
+    tags = ListField(StringField)
+    category = StringField()
+    view_count = IntField()
+    versions = ListField(EmbeddedDocumentField(Version))
